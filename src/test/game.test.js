ec7239da9f71d558a2138e82f5e03fe283e5346e
gonjavi <javiercaliescali@gmail.com>
2020-03-09 13:07:32 -0500

    draw game tests pass

--- src/test/game.test.js
+++ src/test/game.test.js
@@ -1,6 +1,6 @@
 const game = require('../modules/game');
 
-
+// test for win
 test('it returns true if there is a victory', () => {
   expect(game.checkWin(['', '', '', 'O', 'O', 'O', '', '', ''])).toBe(true);
 });
@@ -22,7 +22,6 @@ test('it returns true if there is a victory', () => {
 test('it returns true if there is a victory', () => {
   expect(game.checkWin(['', '', 'O', '', 'O', '', 'O', '', ''])).toBe(true);
 });
-
 test('it returns true if there is a victory', () => {
   expect(game.checkWin(['', '', '', 'X', 'X', 'X', '', '', ''])).toBe(true);
 });
@@ -43,4 +42,17 @@ test('it returns true if there is a victory', () => {
 });
 test('it returns true if there is a victory', () => {
   expect(game.checkWin(['', '', 'X', '', 'X', '', 'X', '', ''])).toBe(true);
+});
+
+test('it returns false if there is not victory', () => {
+  expect(game.checkWin(['', '', '', '', '', '', '', '', ''])).toBe(true);
+});
+
+
+// tests for a draw
+test('it returns true if the game is a draw', () => {
+  expect(game.checkDraw(['X', 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'O'])).toBe(true);
+});
+test('it returns true if the game is a draw', () => {
+  expect(game.checkDraw(['X', 'X', 'X', 'O', 'O', 'X', 'X', 'X', 'O'])).toBe(true);
 });
